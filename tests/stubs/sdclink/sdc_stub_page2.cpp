ef5d3decc856e060920758617ab3da127dc6980f
hnhoan <hnhoan@i-syst.com>
2026-08-10 18:40:47 -0400

    Restore real-header SDC test stub

---
+++ tests/stubs/sdclink/sdc_stub_page2.cpp
@@ -0,0 +1,82 @@
+/* Test-only SDC entry points added separately from the generated stub. */
+#include <stdint.h>
+#include <string.h>
+
+#include "sdc_hci_cmd_controller_baseband.h"
+#include "sdc_hci_cmd_le.h"
+#include "sdc_stub.h"
+
+uint8_t sdc_hci_cmd_cb_set_event_mask_page_2(
+    const sdc_hci_cmd_cb_set_event_mask_page_2_t *pParams)
+{
+    g_SdcStub.LastCall = "sdc_hci_cmd_cb_set_event_mask_page_2";
+    g_SdcStub.Calls++;
+    (void)pParams;
+    return g_SdcStub.NextStatus;
+}
+
+uint8_t sdc_hci_cmd_cb_read_conn_accept_timeout(
+    sdc_hci_cmd_cb_read_conn_accept_timeout_return_t *pReturn)
+{
+    g_SdcStub.LastCall = "sdc_hci_cmd_cb_read_conn_accept_timeout";
+    g_SdcStub.Calls++;
+    if (pReturn != NULL)
+    {
+        memset(pReturn, 0x5A, sizeof(*pReturn));
+    }
+    return g_SdcStub.NextStatus;
+}
+
+uint8_t sdc_hci_cmd_cb_write_conn_accept_timeout(
+    const sdc_hci_cmd_cb_write_conn_accept_timeout_t *pParams)
+{
+    g_SdcStub.LastCall = "sdc_hci_cmd_cb_write_conn_accept_timeout";
+    g_SdcStub.Calls++;
+    (void)pParams;
+    return g_SdcStub.NextStatus;
+}
+
+uint8_t sdc_hci_cmd_le_read_all_local_supported_features(
+    sdc_hci_cmd_le_read_all_local_supported_features_return_t *pReturn)
+{
+    g_SdcStub.LastCall =
+        "sdc_hci_cmd_le_read_all_local_supported_features";
+    g_SdcStub.Calls++;
+    if (pReturn != NULL)
+    {
+        memset(pReturn, 0x5A, sizeof(*pReturn));
+    }
+    return g_SdcStub.NextStatus;
+}
+
+uint8_t sdc_hci_cmd_le_set_host_feature_v2(
+    const sdc_hci_cmd_le_set_host_feature_v2_t *pParams)
+{
+    g_SdcStub.LastCall = "sdc_hci_cmd_le_set_host_feature_v2";
+    g_SdcStub.Calls++;
+    (void)pParams;
+    return g_SdcStub.NextStatus;
+}
+
+uint8_t sdc_hci_cmd_le_set_ext_adv_params_v2(
+    const sdc_hci_cmd_le_set_ext_adv_params_v2_t *pParams,
+    sdc_hci_cmd_le_set_ext_adv_params_v2_return_t *pReturn)
+{
+    g_SdcStub.LastCall = "sdc_hci_cmd_le_set_ext_adv_params_v2";
+    g_SdcStub.Calls++;
+    (void)pParams;
+    if (pReturn != NULL)
+    {
+        memset(pReturn, 0x5A, sizeof(*pReturn));
+    }
+    return g_SdcStub.NextStatus;
+}
+
+uint8_t sdc_hci_cmd_le_ext_create_conn_v2(
+    const sdc_hci_cmd_le_ext_create_conn_v2_t *pParams)
+{
+    g_SdcStub.LastCall = "sdc_hci_cmd_le_ext_create_conn_v2";
+    g_SdcStub.Calls++;
+    (void)pParams;
+    return g_SdcStub.NextStatus;
+}
